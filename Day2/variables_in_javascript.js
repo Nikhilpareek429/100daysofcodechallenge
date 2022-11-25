1b03b4f12a3cd864f14ce21b077cdca734521e18
Nikhil Pareek <118332611+Nikhilpareek429@users.noreply.github.com>
2022-11-25 21:57:35 +0530

    Add files via upload

---
+++ Day2/variables_in_javascript.js
@@ -0,0 +1,15 @@
+console.log("Variable In Java Script : var , let and const")
+//var a = 45;
+//var b = "Nikhil"
+var c = null;
+var d = undefined;
+//console.log(a,b,c,d);
+/*{
+  var b = 'this'
+  console.log(b);
+}*/
+//var can be updated & re-declared within its scope 
+
+let a = "Nikhil";
+let b = 29;
+console.log(a,b,c,d);
