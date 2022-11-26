d4712deda17027b0d9d4886ae1e33170883da0eb
Nikhil Pareek <118332611+Nikhilpareek429@users.noreply.github.com>
2022-11-26 22:05:03 +0530

    Add files via upload

---
+++ Day3/permitiveObjects.js
@@ -0,0 +1,19 @@
+// nn bb ss u
+let a = null;
+let b = 345;
+let c = true; // Can also be false
+let d = BigInt("567")
+let e = "Harry"
+let f = Symbol("I am a nice symbol");
+let g = undefined
+console.log(a, b, c, d, e, f, g);
+console.log(typeof c);
+
+// Objects in Js
+const item = {
+  "Harry": true,
+  "Shubh": false,
+  "Lovish": 67,
+  "Rohan": undefined
+}
+console.log(item["Shubh"])
