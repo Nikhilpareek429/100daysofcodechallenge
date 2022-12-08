0202198a831028deae960635fda2553e6caeab55
Nikhil Pareek <118332611+Nikhilpareek429@users.noreply.github.com>
2022-12-08 21:44:15 +0530

    Add files via upload

---
+++ Day9/whileloop.js
@@ -0,0 +1,19 @@
+// while loop
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+
+let i = 0;
+while (i < n) {
+  console.log(i)
+  i++;
+}
+
+// while loop
+let a = prompt("Enter the value of n")
+a = Number.parseInt(a)
+
+let b = 0;
+do {
+  console.log(b)
+  b++;
+} while (b < a) 
