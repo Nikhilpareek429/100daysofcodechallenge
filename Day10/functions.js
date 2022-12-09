6c5e30ae1b4be7e1082aa6147d13d324ae41168d
Nikhil Pareek <118332611+Nikhilpareek429@users.noreply.github.com>
2022-12-09 21:59:20 +0530

    Add files via upload

---
+++ Day10/functions.js
@@ -0,0 +1,27 @@
+function onePlusAvg(x, y)
+  {
+    return 1 + (x + y)/2
+  }
+
+const sum = (p, q)=>
+  {
+  return p + q
+  }
+
+const hello = ()=>{
+  console.log("Hey how are you ??")
+}
+
+
+let a = 1;
+let b = 4;
+let c = 5;
+
+console.log("One plus Average of a and b is ", onePlusAvg(a, b));
+console.log("One plus Average of b and c is ", onePlusAvg(b, c));
+console.log("One plus Average of c and a is ", onePlusAvg(c, a));
+
+console.log (sum(55,44));
+console.log (sum(12456,98754));
+
+hello();
