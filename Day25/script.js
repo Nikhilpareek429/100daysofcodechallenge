9f9579a063face98cd61c7f367ef82bae23a51a6
Nikhil Pareek <118332611+Nikhilpareek429@users.noreply.github.com>
2023-03-20 21:39:23 +0530

    Add files via upload

---
+++ Day25/script.js
@@ -0,0 +1,9 @@
+//Change the card title to red
+let ctitle = document.getElementById("firstcardtitle")
+ctitle.style.color = "blue"
+
+let ctitles = document.querySelectorAll(".card-title")
+ctitles[0].style.color = "blue"
+ctitles[1].style.color = "red"
+ctitles[2].style.color = "blacki"
+console.log(ctitles)
