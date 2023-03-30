a2f028ef08e4ce9ba905d95d7a7e07b34d132e32
Nikhil Pareek <118332611+Nikhilpareek429@users.noreply.github.com>
2023-03-30 21:58:14 +0530

    Add files via upload

---
+++ Day34/script.js
@@ -0,0 +1,27 @@
+document.getElementById("google").addEventListener("click", function() {
+    window.location = "https://www.google.com";
+    win.focus();
+  })
+  // document.getElementById("fb").addEventListener("click", function() {
+  //   window.location = "https://www.fb.com";
+  //   win.focus();
+  // })
+  // document.getElementById("twitter").addEventListener("click", function() {
+  //   window.location = "https://www.twitter.com";
+  //   win.focus();
+  // })
+  
+  // const fetchContent = async (url) => {
+  //   con = await fetch(url);
+  //   let a = await con.json()
+  //   return a;
+  // }
+  
+  // setInterval(async function() {
+  //   let url = "https://jsonplaceholder.typicode.com/todos/1"
+  //   console.log(await fetchContent(url))
+  // }, 3000)
+  
+  // setInterval(async function() {
+  //   document.querySelector("#bulb").classList.toggle("bulb")
+  // }, 300)
