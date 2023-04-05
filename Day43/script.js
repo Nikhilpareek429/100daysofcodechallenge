20830b807effe8ebcf2d5e00481aff0f33b85f3a
Nikhil Pareek <118332611+Nikhilpareek429@users.noreply.github.com>
2023-04-05 16:20:06 +0530

    Add files via upload

---
+++ Day43/script.js
@@ -0,0 +1,24 @@
+setTimeout(()=>{
+    console.log("Hacking wifi....Please wait....")
+  }, 1000)
+  
+  try{
+    console.log(rahul)
+  }
+  catch(error){
+    console.log("Wait after some time")
+  }
+  
+  
+  setTimeout(()=>{
+    console.log("Fetching username and password....... Please wait..." )
+  }, 2000)
+  
+  setTimeout(()=>{
+    console.log("Hacking Rahul's facebook id....... Please wait..." )
+  }, 3000)
+  
+  setTimeout(()=>{
+    console.log("username and password of Rahul (+91333040409)....... Please wait..." )
+  }, 4000)
+  
